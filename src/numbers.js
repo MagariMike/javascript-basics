7dd4a64ae0964c59fb3a9112b962c50397a496b0
Mike Brewer <michaelandrewbrewer@gmail.com>
2022-11-30 18:49:23 +0000

    numbers started

--- src/numbers.js
+++ src/numbers.js
@@ -7,39 +7,39 @@ function subtract (a, b) {
 }
 
 function multiply (a, b) {
-  // your code here
+  return a * b;
 }
 
 function divide (a, b) {
-  // your code here
+ return a / b;
 }
 
 function power (a, b) {
-  // your code here
+  return Math.pow (a,b);
 }
 
 function round (a) {
-  // your code here
+  return Math.round (a);
 }
 
 function roundUp (a) {
-  // your code here
+  return Math.ceil(a);
 }
 
 function roundDown (a) {
-  // your code here
+  return Math.floor(a);
 }
 
 function absolute (a) {
-  // your code here
+  return Math.abs(a);
 }
 
 function quotient (a, b) {
-  // your code here
+  
 }
 
 function remainder (a, b) {
-  // your code here
+
 }
 
 module.exports = {
