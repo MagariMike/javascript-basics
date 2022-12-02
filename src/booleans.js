3e6ae7fb6b6360daf37b102ea6792380adeb2961
Mike Brewer <michaelandrewbrewer@gmail.com>
2022-12-02 08:24:49 +0000

    working on booleans

--- src/booleans.js
+++ src/booleans.js
@@ -1,41 +1,68 @@
 function negate(a) {
-  // your code here
+  return !a;
 };
 
 function both(a, b) {
-  // your code here
+  return a && b;
+
 };
 
 function either(a, b) {
-  // your code here
+  return a || b;
 };
 
 function none(a, b) {
-  // your code here
+  if (a === false && b === false) {
+    return true;
+  }
+  else {
+    return false;
+  }
 };
 
 function one(a, b) {
-  // your code here
+return Boolean(a ^ b);
+// NEED TO REVIEW THE ABOVE*//
 };
 
 function truthiness(a) {
-  // your code here
+  return Boolean(a);
 };
 
 function isEqual(a, b) {
-  // your code here
+  if (a === b){
+    return true;
+  }
+  else {
+    return false;
+  }
 };
 
 function isGreaterThan(a, b) {
-  // your code here
+  if (a > b){
+    return true;
+  }
+  else {
+    return false;
+  }
 };
 
 function isLessThanOrEqualTo(a, b) {
-  // your code here
+  if (a <= b){
+    return true;
+  }
+  else {
+    return false;
+  }
 };
 
 function isOdd(a) {
-  // your code here
+  if (a % 2 === 1){
+    return true;
+  }
+  else {
+    return false;
+  }
 };
 
 function isEven(a) {
