f6995fe303ed43d3357e22584c71c441e2811467
Mike Brewer <michaelandrewbrewer@gmail.com>
2022-12-06 22:15:51 +0000

    objects completed half

--- src/objects.js
+++ src/objects.js
@@ -1,25 +1,38 @@
 const createPerson = (name, age) => {
-  // your code here
+  const person = {
+    name: name,
+    age: age,
+  };
+  return person;
 };
 
 const getName = object => {
-  // your code here
+  return object.name;
 };
 
 const getProperty = (property, object) => {
-  // your code here
+  return object[property];
 };
 
 const hasProperty = (property, object) => {
-  // your code here
+  if (object.hasOwnProperty(property)) {
+    return true;
+  }
+  return false;
 };
 
 const isOver65 = person => {
-  // your code here
+  if (person.age > 65) {
+    return true;
+  }
+  return false;
 };
 
 const getAges = people => {
-  // your code here
+  const ages = people.map((person) => { 
+    return person.age;
+  });
+  return ages;
 };
 
 const findByName = (name, people) => {
